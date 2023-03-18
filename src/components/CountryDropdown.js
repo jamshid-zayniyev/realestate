b307ede40f71701c7cd89e013e2e65fa7f861ab4
jamshid-zayniyev <jamshidzayniev10@gmail.com>
2023-03-18 18:51:53 +0500

    xasda
    dsada

--- src/components/CountryDropdown.js
+++ src/components/CountryDropdown.js
@@ -4,8 +4,33 @@ import {Menu} from "@headlessui/react"
 import { HouseContext } from './HouseContext';
 const CountryDropdown = () => {
   const {country,setCountry,countries} = useContext(HouseContext)
-  console.log(country);
-  return <div>CountryDropdown</div>;
+  console.log(countries);
+  const [isOpen, setIsOpen] = useState(false)
+  return <Menu as="div" className="dropdown">
+    <Menu.Button className="dropdown-btn w-full text-left" onClick={()=>setIsOpen(!isOpen)}>
+      <RiMapPinLine className='dropdown-icon-primary'/>
+      <div>
+        <div className='text-[15px] font-medium leading-tight'>{country}</div>
+        <div className='text-[13px]'>Select your place</div>
+      </div>
+
+        {
+          isOpen ? (
+            <RiArrowUpSLine className='dropdown-icon-secondary'/>
+          ): (
+            <RiArrowDownSLine className='dropdown-icon-secondary'/>
+          )
+        }
+    </Menu.Button>
+
+    <Menu.Items>
+      {countries.map((country,index)=>{
+        return <Menu.Item className="cursor-pointer hover:text-violet-700 transition" as="li" key={index}>
+          {country}
+        </Menu.Item>
+      })}
+    </Menu.Items>
+  </Menu>;
 };
 
 export default CountryDropdown;
