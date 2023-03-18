d790fd83318aacc5d2ebeeda9e2467b8e515c3dc
jamshid-zayniyev <jamshidzayniev10@gmail.com>
2023-03-18 17:07:27 +0500

    required
    required

--- src/components/CountryDropdown.js
+++ src/components/CountryDropdown.js
@@ -1,6 +1,10 @@
-import React from 'react';
-
+import React, {useState,useEffect, useContext}from 'react';
+import {RiMapPinLine,RiArrowDownSLine,RiArrowUpSLine} from "react-icons/ri"
+import {Menu} from "@headlessui/react"
+import { HouseContext } from './HouseContext';
 const CountryDropdown = () => {
+  const {country,setCountry,countries} = useContext(HouseContext)
+  console.log(country);
   return <div>CountryDropdown</div>;
 };
 
--- src/components/HouseContext.js
+++ src/components/HouseContext.js
@@ -1,10 +1,27 @@
 import React, {useState, useEffect,useContext, createContext} from 'react';
-import {houseData} from "../data"
-
+import {housesData} from "../data"
 export const HouseContext = createContext();
 
 const HouseContextProvider = ({children}) => {
-  return <HouseContext.Provider value={console.log('this is the context')}>
+  const [houses,setHouses] = useState(housesData)
+  const [country,setCountry] = useState('Location (any)')
+  const [countries,setCountries] = useState([])
+  const [property,setProperty] = useState('Property type (any)')
+  const [properties,setProperties] = useState([])
+  const [price,setPrice] = useState('Price range (any)')
+  const [loading,setLoading] = useState(false)
+  return <HouseContext.Provider value={{
+    country,
+    setCountry,
+    countries,
+    property,
+    setProperty,
+    properties,
+    price,
+    setPrice,
+    houses,
+    loading,
+  }}>
     {children}
   </HouseContext.Provider>;
 };
